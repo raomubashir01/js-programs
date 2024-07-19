496a9d83222742213bd7aaec09cdcf19bf81d4fd
raomubashir01 <mubashirkhaliq147@gmail.com>
2024-07-19 18:51:52 +0000

    01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID =12345
+let accountemail ="muabhsir@gmail.com"
+accountPassword = "1234"
+accountcity = "jaipur"
+ 
+let accountstate 
+
+console.table([accountID , accountemail , accountPassword , accountcity , accountstate  ])
+
+
+// let message;
+// message = 'Hello!';
+
+// alert(message)
