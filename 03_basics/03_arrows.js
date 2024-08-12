89b19d48fb2e40d8551a86eb3314c0d7a01a8101
raomubashir01 <mubashirkhaliq147@gmail.com>
2024-08-12 21:07:48 +0000

    arrows.js

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,84 @@
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        // console.log(`${this.username} , welcome to website`);
+        // console.log(this);
+    }
+
+}
+
+
+// user.username = "sam"
+//  user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+    let username = "hitesh"
+    // console.log(this);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    // console.log(this);
+} 
+chai()
+
+const aurcode = () => {
+    let username = "mubashir"
+    // console.log(this);
+}
+aurcode()
+
+// chai()
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+// console.log (addTwo (3 ,4))
+cosole.log (addTwo (2 , 3))
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
+
+
+
+
+
+//++++++++++++++++++ practice +++++++++++++++++++++++
+
+// const User = {
+//     username: " mubashir",
+//     price: 199 ,
+
+//     welcomeMessage: function () {
+//         console.log (`${this.username} ,welcome to website`)
+//         console.log(this);
+//     }
+
+// }
+// User.welcomeMessage()
+// User.username = "sam"
+// User.welcomeMessage()
+
