8e90b9f4be88a51b98a17860639a5646d480a804
raomubashir01 <mubashirkhaliq147@gmail.com>
2024-09-04 16:46:11 +0000

    while loop

---
+++ 05_iterations/while.js
@@ -0,0 +1,43 @@
+
+let index = 0 
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+// let score = 11
+
+// do {
+//     // console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+
+///////////////////////////////// practice 
+ 
+// let  score = 0
+
+// do {
+//     // console.log(`score is ${score}`)
+//      score = score + 2
+// } while (score <=10);
+
+
+// let score = 0
+// do {console.log(` score is ${score}`)
+//     score = score + 2  
+//     if (score == 10 ){
+//         // console.log(`detected 10 ${score}`)
+//     } 
+// } while (score <= 20  );
+ 
+
+
